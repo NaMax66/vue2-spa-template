f22a7352c7b9741d86aa1f448408a0d61d1fad67
Max <max.naid@mail.ru>
2021-04-24 23:34:15 +0500

    eslint config

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+<button>{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton.vue',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <base-button @click.native="test" text="Купть" />
   </div>
 </template>
+<script lang="ts">
+import BaseButton from '@/components/base/BaseButton.vue'
+
+export default {
+  components: { BaseButton },
+  methods: {
+    test(): void {
+      console.log('hi')
+    }
+  }
+}
+</script>
